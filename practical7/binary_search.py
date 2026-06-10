e7b20531f9dbb55feb419ddde3cf4418908bba64
nattttts <natalieshee@gmail.com>
2026-06-10 17:31:42 +0800

    Add code for quick sort and binary search

--- practical7/binary_search.py
+++ practical7/binary_search.py
@@ -7,3 +7,26 @@
 # Return the index.
 # Expected Output:
 # Found at index 2
+
+def binary_search(arr, target):
+    left = 0
+    right = len(arr) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1  # not found
+
+
+binary_search_list = [8, 12, 31, 46, 57, 214]
+target_no = 31
+
+result = binary_search(binary_search_list, target_no)
+print("Found at index", result)
--- practical7/quick_sort.py
+++ practical7/quick_sort.py
@@ -5,31 +5,34 @@
 # Expected Output:
 # [8, 12, 31, 46, 57, 214]
 
-def swap(x, y):
-    temp = x
-    x = y
-    y = temp
+quick_sort_list = [214, 12, 46, 57, 31, 8]
+
+def swap(a, i, j):
+    a[i], a[j] = a[j], a[i]
 
 
 def partition(a, first, last):
     # use first item as pivot
 
     pivot = a[first]
-    current_index = first
+    i = first
 
-    m = first + 1
+    for j in range(first + 1, last + 1):
+        if a[j] < pivot:
+            i += 1
+            swap(a, i, j)
 
-    while m <= last:
-        if a[m] < pivot:
-            current_index += 1
-            swap(a[m], a[current_index])
-        m += 1
-
-    swap(a[first], a[current_index])
-    return current_index
+    swap(a, first, i)
+    return i
 
 
 def quick_sort(a, first, last):
 
     if first < last:
         pivot_index = partition(a, first, last)
+        quick_sort(a, first, pivot_index - 1)
+        quick_sort(a, pivot_index + 1, last)
+
+
+quick_sort(quick_sort_list, 0, len(quick_sort_list) - 1)
+print(quick_sort_list)
